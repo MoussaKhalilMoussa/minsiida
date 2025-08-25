c1b2fa61d3d438ce4cdc32894e74030b6e83c725
MoussaKhalilMoussa <moussa.khalil.92@gmail.com>
2025-08-25 17:14:50 +0300

    message in developpemnt

--- lib/view/profile/pages/messages_page.dart
+++ lib/view/profile/pages/messages_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:simple_nav_bar/constants/colors.dart';
+import 'package:simple_nav_bar/view/profile/widgets/favori_product_card.dart';
 
 class MessagesPage extends StatelessWidget {
   const MessagesPage({super.key});
@@ -6,8 +11,111 @@ class MessagesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Messages')),
-      body: const Center(child: Text('This is the Messages Page')),
+      backgroundColor: Colors.grey[200],
+      body: CustomScrollView(
+        //physics: BouncingScrollPhysics(),
+        shrinkWrap: true,
+        slivers: [
+          SliverAppBar(
+            backgroundColor: whiteColor,
+            pinned: true,
+            leading: IconButton(
+              icon: const Icon(Ionicons.chevron_back_outline),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            centerTitle: true,
+            title: Text(
+              'Mes Messages',
+              style: GoogleFonts.playfairDisplay(fontWeight: FontWeight.bold),
+            ),
+            floating: true,
+            snap: true,
+          ),
+          const SliverToBoxAdapter(child: Divider(height: 1)),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Container(
+                width: MediaQuery.sizeOf(Get.context!).width,
+                margin: EdgeInsets.only(right: 12, left: 12, top: 20),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      width: MediaQuery.sizeOf(Get.context!).width * 0.93,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          //focusColor: greyColor,
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: greyColor, width: 1),
+                          ),
+                          filled: true,
+                          fillColor: whiteColor,
+                          hintText: "Rechercher des messages...",
+                          prefixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 10, right: 10),
+                width: MediaQuery.sizeOf(Get.context!).width,
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        radius: 25,
+                        backgroundImage: AssetImage(
+                          'assets/images/profile_image_joya_ahsan.jpg',
+                        ),
+                      ),
+                      title: Text(
+                        'Utilisateur $index',
+                        style: GoogleFonts.playfairDisplay(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      subtitle: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Dernier message de l\'utilisateur $index',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: GoogleFonts.playfairDisplay(
+                              color: greyColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text('12:34 PM'),
+                          CircleAvatar(radius: 4, backgroundColor: blueColor),
+                        ],
+                      ),
+                      onTap: () {
+                        // Action lors du tap sur un message
+                      },
+                    );
+                  },
+                  itemCount: 10,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                ),
+              ),
+            ]),
+          ),
+        ],
+      ),
     );
   }
 }
